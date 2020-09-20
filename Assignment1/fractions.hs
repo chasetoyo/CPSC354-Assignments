74a40c928d7944c7be535a0bbd650e9aa1d98fc0
Chase Toyofuku-Souza <chasetoyo@gmail.com>
2020-09-20 12:45:58 -0700

    pos numbers done, need runtime error n2p

--- Assignment1/fractions.hs
+++ Assignment1/fractions.hs
@@ -64,93 +64,123 @@ gcdN O n = n
 gcdN n O = n
 gcdN n m = gcdN m (modl n m)
 
--------------------
--- POSITIVE NUMBERS
--------------------
 
+-- -----------------
+-- POSITIVE NUMBERS
+-- -----------------
+
+-- I represents 1
+-- T represents any number of 1's preceeding I
 data PN = I | T PN
     deriving (Eq,Show)
 
 -- addition of positive numbers
 -- use recursion over PN
+-- same as addition over natural numbers, get rid of identity law
 addP :: PN -> PN -> PN
+addP I n = T n
+addP (T n) m = T (addP n m)
 
 -- multiplication of positive numbers
 -- use recursion over PN
 multP :: PN -> PN -> PN
+multP I n = n
+multP n I = n
+multP (T n) m = addP m (multP n m)
 
 -- lessP n m if n < m
 -- use recursion over PN
 lessP :: PN -> PN -> Bool
+lessP I I = False
+lessP I n = True
+lessP n I = False
+lessP (T n) (T m) = lessP n m
 
 -- subtract a positive number from a natural number
 -- use recursion over NN
 subtrNP :: NN -> PN -> NN
+subtrNP O n = O
+subtrNP (S n) I = subtr (S n) (S O)
+subtrNP (S n) (T m) = subtrNP n m
 
 -- divide a natural number by a positive number
 -- use recursion over NN
+-- anything divided by 1 is itself
+    -- otherwise, same as divi
 divP :: NN -> PN -> NN
+divP O n = O
+divP n I = n
+divP (S n) (T m) =
+    if less (S n) (p2n (T m))
+        then O
+        else
+            divP (subtrNP n m) m
 
 -- convert from PN to NN
 -- use recursion over PN
 p2n :: PN -> NN
+p2n I = (S O)
+p2n (T m) = S(p2n m)
 
 -- convert from NN to PN
 -- use recursion over NN
 -- allow runtime error if NN is O
 n2p :: NN ->PN
+n2p (S O) = I
+n2p (S n) = T (n2p n)
 
-------------
--- FRACTIONS
-------------
 
--- non-negative fractions
--- a fraction is a pair (numerator,denominator)
-type Frac = (NN,PN)
+-- ------------
+-- -- FRACTIONS
+-- ------------
 
--- multiply fractions
--- recall from school how to multiply fractions
-multF :: Frac -> Frac -> Frac
+-- -- non-negative fractions
+-- -- a fraction is a pair (numerator,denominator)
+-- type Frac = (NN,PN)
 
--- add fractions
--- recall from school how to add fractions
-addF :: Frac -> Frac -> Frac
+-- -- multiply fractions
+-- -- recall from school how to multiply fractions
+-- multF :: Frac -> Frac -> Frac
 
--- equality of fractions
--- recall from school how to check that two fractions are equal
-equalF :: Frac -> Frac -> Bool
+-- -- add fractions
+-- -- recall from school how to add fractions
+-- addF :: Frac -> Frac -> Frac
 
--- simplify fractions
--- divide numerator and denominator by the gcd of both
-simplifyF :: Frac -> Frac
+-- -- equality of fractions
+-- -- recall from school how to check that two fractions are equal
+-- equalF :: Frac -> Frac -> Bool
 
---------------
--- FOR TESTING
---------------
+-- -- simplify fractions
+-- -- divide numerator and denominator by the gcd of both
+-- simplifyF :: Frac -> Frac
 
--- use recursion on NN
-nn2int :: NN -> Int
+-- --------------
+-- -- FOR TESTING
+-- --------------
 
--- use recursion on Int
--- allow runtime error for negative numbers
-int2nn :: Int -> NN
+-- -- use recursion on NN
+-- nn2int :: NN -> Int
 
--- use recursion on Int
--- allow runtime error for non-positive numbers
-int2pn :: Int -> PN
+-- -- use recursion on Int
+-- -- allow runtime error for negative numbers
+-- int2nn :: Int -> NN
 
--- use int2nn and int2pn
-ints2frac :: (Int,Int) -> Frac
-ints2frac (n,p) = (int2nn n, int2pn p)
+-- -- use recursion on Int
+-- -- allow runtime error for non-positive numbers
+-- int2pn :: Int -> PN
 
--- use nn2int 
-frac2ints :: Frac -> (Int,Int)
-frac2ints x = (nn2int (fst x), nn2int(p2n(snd x)))
+-- -- use int2nn and int2pn
+-- ints2frac :: (Int,Int) -> Frac
+-- ints2frac (n,p) = (int2nn n, int2pn p)
 
--- Some examples (make your own):
--- 
--- frac2ints (addF (ints2frac (2,3)) (ints2frac (6,8)))
--- equalF (ints2frac (2,6)) (ints2frac (1,3))
--- addF (ints2frac (36,60)) (ints2frac (24,45))
--- simplifyF (addF (ints2frac (36,60)) (ints2frac (24,45)))
--- frac2ints(simplifyF (addF (ints2frac (36,60)) (ints2frac (24,45))))
+-- -- use nn2int 
+-- frac2ints :: Frac -> (Int,Int)
+-- frac2ints x = (nn2int (fst x), nn2int(p2n(snd x)))
+
+-- -- Some examples (make your own):
+-- -- 
+-- -- frac2ints (addF (ints2frac (2,3)) (ints2frac (6,8)))
+-- -- equalF (ints2frac (2,6)) (ints2frac (1,3))
+-- -- addF (ints2frac (36,60)) (ints2frac (24,45))
+-- -- simplifyF (addF (ints2frac (36,60)) (ints2frac (24,45)))
+-- -- frac2ints(simplifyF (addF (ints2frac (36,60)) (ints2frac (24,45))))
