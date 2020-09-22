4b981093451554fb0d2812dd0979de096571a18a
Chase Toyofuku-Souza <chasetoyo@gmail.com>
2020-09-21 20:44:59 -0700

    remove power, add decrement

--- Assignment1/Part2/numbers3.hs
+++ Assignment1/Part2/numbers3.hs
@@ -2,7 +2,7 @@
 
 -- a language for arithmetic expressions
 data Exp = Num Integer | Plus Exp Exp | Times Exp Exp | Minus Exp Exp | Divide Exp Exp |
-           Modulo Exp Exp | Power Exp Exp | Equals Exp Exp | Increment Exp
+           Modulo Exp Exp | Equals Exp Exp | Increment Exp | Decrement Exp
 
 eval :: Exp -> Integer
 eval (Num n) = n
@@ -14,8 +14,7 @@ eval (Minus e e') = (eval e) - (eval e')
 -- `div` gives a floor division
 eval (Divide e e') = (eval e) `div` (eval e') 
 eval (Modulo e e') = (eval e) `mod` (eval e')
--- '^' is the symbol to use exponents with integers
-eval (Power e e') = (eval e) ^ (eval e')
--- technically a pre increment
--- eval (Plus (Num 2) (Increment (Num 3))) results in 6
+-- pre increment
 eval (Increment e) = (eval e) + 1
+-- pre decrement
+eval (Decrement e) = (eval e) - 1
