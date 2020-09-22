12f284a5acae0cdf328393971362576ea7569ebd
Chase Toyofuku-Souza <chasetoyo@gmail.com>
2020-09-21 20:45:25 -0700

    Implement grammar and interpreter

--- Assignment1/Part2/Calculator/Interpreter.hs
+++ Assignment1/Part2/Calculator/Interpreter.hs
@@ -6,3 +6,8 @@ eval :: Exp -> Integer
 eval (Num n) = n
 eval (Plus n m) = (eval n) + (eval m)
 eval (Times n m) = (eval n) * (eval m)
+eval (Divide n m) = (eval n) `div` (eval m)
+eval (Minus n m) = (eval n) - (eval m)
+eval (Modulo n m) = (eval n) `mod` (eval m)
+eval (Increment n) = (eval n) + 1
+eval (Decrement n) = (eval n) - 1
